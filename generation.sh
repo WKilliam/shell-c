1df0a04d8a6550834e0b59e5ce91621c099ba978
ted <william.k97430@gmail.com>
2020-04-23 14:20:20 +0200

    log acces error

--- generation.sh
+++ generation.sh
@@ -9,7 +9,7 @@ valueCountE=0
 ./tick $1 | ./sensorData 2>&1 | {
 	while IFS= read -r RAW_LINE;do
 
-		#echo "la ligne = $RAW_LINE"
+		echo "la ligne = $RAW_LINE"
 
 		raw=$(echo $RAW_LINE | cut -d ";" -s -f4)
 
@@ -20,10 +20,24 @@ valueCountE=0
 
 		echo "$raw"
 
-		if [[ $raw == M* ]]; then
-			echo $RAW_LINE >> good.log
+		if [[ $raw == m* ]]; then
+
+			text1=$(echo $RAW_LINE | cut -d ";" -s -f1)
+
+			text2=$(echo $RAW_LINE | cut -d ";" -s -f2)
+
+			text3=$(echo $RAW_LINE | cut -d ";" -s -f3)
+
+			text4=$(echo $RAW_LINE | cut -d ";" -s -f5)
+
+			mytext=$(echo "SensorID : $text1 , SensorName : $text2 , Value : $text3 , MaxValue : $text4 ")
+
+
+			echo $mytext  > acces.log
+
+		else 
+			echo $RAW_LINE > error.log
 		fi
 
 	done
 }
-#./tick $1 1> value  | ./sensorData value
---
+++ supervision.sh
@@ -0,0 +1,7 @@
+#!\bin\bash
+
+valueTickTake=$1
+valuefileAcces=$2
+valuefileError=$3
+
+./generation.sh
