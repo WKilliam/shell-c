4af4e9ce4752770d55f7ae62a964ee365512f5da
ted <william.k97430@gmail.com>
2020-04-23 12:17:59 +0200

    story6

--- generation.sh
+++ generation.sh
@@ -2,11 +2,28 @@
 
 #this variable take the first arguments of generation.sh
 valueTick=$1
+valueCountG=0
+valueCountE=0
 
 #./tick $1 | ./sensorData 2>&1
 ./tick $1 | ./sensorData 2>&1 | {
 	while IFS= read -r RAW_LINE;do
-		echo "ligne = $RAW_LINE"
+
+		#echo "la ligne = $RAW_LINE"
+
+		raw=$(echo $RAW_LINE | cut -d ";" -s -f4)
+
+		#echo "ici je test raw = $raw" 
+
+		#echo $RAW_LINE | cut -d ";" -s -f4
+
+
+		echo "$raw"
+
+		if [[ $raw == M* ]]; then
+			echo $RAW_LINE >> good.log
+		fi
+
 	done
 }
 #./tick $1 1> value  | ./sensorData value
