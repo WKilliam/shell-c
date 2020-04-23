4fba959c719eae9e3e219429036126c5c5b82f58
ted <william.k97430@gmail.com>
2020-04-23 17:28:09 +0200

    story 8

--- generation.sh
+++ generation.sh
@@ -2,6 +2,9 @@
 
 #this variable take the first arguments of generation.sh
 valueTick=$1
+valuefile=$2
+valueAcces=$3
+valuefileError=$4
 valueCountG=0
 valueCountE=0
 mkdir -p $HOME/log
@@ -10,7 +13,7 @@ mkdir -p $HOME/log
 ./tick $1 | ./sensorData 2>&1 | {
 	while IFS= read -r RAW_LINE;do
 
-		echo "la ligne = $RAW_LINE"
+		#echo "la ligne = $RAW_LINE"
 
 		raw=$(echo $RAW_LINE | cut -d ";" -s -f4)
 
@@ -19,7 +22,7 @@ mkdir -p $HOME/log
 		#echo $RAW_LINE | cut -d ";" -s -f4
 
 
-		echo "$raw"
+		#echo "$raw"
 
 		if [[ $raw == m* ]]; then
 
@@ -33,10 +36,10 @@ mkdir -p $HOME/log
 
 			mytext=$(echo "User : $USER , IDuser : $INVOCATION_ID  , SensorID : $text1 , SensorName : $text2 , Value : $text3 , MaxValue : $text4 ")
 
-			echo $mytext  > $HOME/log/acces.log
+			echo $mytext >> $HOME/$2/$3
 
 		else 
-			echo $RAW_LINE > $HOME/log/error.log
+			echo $RAW_LINE >> $HOME/$2/$4
 		fi
 
 	done
--- supervision.sh
+++ supervision.sh
@@ -1,7 +1,23 @@
-#!\bin\bash
+#!/bin/bash
 
-valueTickTake=$1
-valuefileAcces=$2
-valuefileError=$3
+valueTick=$1
+dossiervalue=$2
+fileAcces=$3
+fileError=$4
 
-./generation.sh
+./generation.sh $1 $2 $3 $4 &
+
+valuetext=sudo ls -l $HOME/$2/$3 | cut -d " " -f5
+
+#valuetails=$($HOME/log/acces.log | cut -d " " -f5) 
+
+#echo $valuetext
+while $valuetext < 50000;do
+if [[ $valuetext < 2000 ]];then
+echo $valuetext
+fi
+done
+
+#if [[ "$valuetails" > 200 ]];then
+#	echo "YOOOOOOOOOOO"
+#fi
